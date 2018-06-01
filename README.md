99702665395e13cdfb8b50be1103b8bef5c0272a
moTzxx <JingYin007@users.noreply.github.com>
2018-06-01 15:19:13 +0800

    测试框架
    进行微信小程序学习的测试框架初始化

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# snacksWx
-微信的一个 零食小程序开发，需要配合 wxTp5Pro 服务端进行数据获取
+# protoss-tool
--- utils/util.js
+++ utils/util.js
@@ -1,19 +1,32 @@
-const formatTime = date => {
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const day = date.getDate()
-  const hour = date.getHours()
-  const minute = date.getMinutes()
-  const second = date.getSeconds()
+function formatTime(date) {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds()
+
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
-const formatNumber = n => {
+function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
+function requestBanner(callback){
+  wx.request({
+    url: 'http://wxzergpro.com/api/v1/banner/1',
+    method: 'GET',
+    success: function (data) {
+       callback(data);
+    },
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  requestBanner: requestBanner
 }
